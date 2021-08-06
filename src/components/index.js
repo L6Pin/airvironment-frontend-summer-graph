f1bf8906cd5106e9c921865144fceb896c1fafa0
Luka Peric <lukaperickv@gmail.com>
2021-08-06 20:13:50 +0200

    Chart component code refactor

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import MeasurementsCard from "./MeasurementsCard";
 import MeasurementsFilter from "./MeasurementsFilter";
+import Chart from "./Chart";
 
-export { Header, MeasurementsCard, MeasurementsFilter };
+export { Header, MeasurementsCard, MeasurementsFilter, Chart };
